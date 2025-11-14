3f66df108287fac6e3ffebd9da38ae7e3582f368
Sub2Ninah <70987554+krstacker@users.noreply.github.com>
2025-11-14 15:08:32 +0800

    Update sega-rotate.js

--- script/game/loop-modules/sega-rotate.js
+++ script/game/loop-modules/sega-rotate.js
@@ -2,7 +2,7 @@ import input from "../../input.js"
 
 export default function segaRotate(arg) {
   const piece = arg.piece
-  if (piece.y < 4 && this.shape === "I") {
+  if (piece.y < 4 && piece.shape === "I") {
 	  return
   } else if (piece.y < 2) {
 	  return
